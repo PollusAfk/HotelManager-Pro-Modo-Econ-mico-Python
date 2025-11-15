33f120235dc104701bc57755f53a40a621501c2d
Pollus <67242058+PollusAfk@users.noreply.github.com>
2025-11-15 12:05:21 -0300

    Update Hotel_manager.py

--- Hotel_manager.py
+++ Hotel_manager.py
@@ -1,8 +1,18 @@
-numeros_quartos = list(range(101, 151))
-status_quartos=["livre", "ocupado", "manutencao", "reservado"]
-hospedes_quartos=[]
-dias_estadia=[]
+numeros_quartos = list(range(101, 151))  # de 101 a 150
+status_quartos = []
+hospedes_quartos = []
+dias_estadia = []
 
-def inicializar_hotel():
+def status_dosQuarto():
+    contador = 0
+    while contador < 50:
+        status_quartos.append("livre")
+        hospedes_quartos.append("")
+        dias_estadia.append(0)
+        contador += 1
+    print("Status dos quartos inicializado com sucesso.")
+print("Quarto:", numeros_quartos[i])
+print("Status:", status_quartos[i])
+print("Hóspede:", hospedes_quartos[i])
+print("Dias:", dias_estadia[i])
 
-def 
